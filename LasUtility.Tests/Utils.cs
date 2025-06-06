d06fa9363082b23f86635e4478a337b68b7ecb0b
Vellu Sorvari <vepeli@gmail.com>
2025-06-06 13:54:32 +0300

    LasUtility.Tests: Converting line endings when comparing files

--- LasUtility.Tests/Utils.cs
+++ LasUtility.Tests/Utils.cs
@@ -1,62 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace LasUtility.Tests
 {
-    internal class Utils
+    internal static class Utils
     {
         internal static bool FileCompare(string sFile1, string sFile2)
         {
-            int file1byte;
-            int file2byte;
-            FileStream fs1;
-            FileStream fs2;
-
-            // Determine if the same file was referenced two times.
-            if (sFile1 == sFile2)
+            try
             {
-                // Return true to indicate that the files are the same.
-                return true;
+                using var reader1 = File.ReadLines(sFile1).GetEnumerator();
+                using var reader2 = File.ReadLines(sFile2).GetEnumerator();
+
+                while (reader1.MoveNext() && reader2.MoveNext())
+                {
+                    if (NormalizeLineEndings(reader1.Current) != NormalizeLineEndings(reader2.Current))
+                    {
+                        return false;
+                    }
+                }
+
+                // Ensuring both files are completely read
+                return !reader1.MoveNext() && !reader2.MoveNext();
             }
-
-            // Open the two files.
-            fs1 = new FileStream(sFile1, FileMode.Open);
-            fs2 = new FileStream(sFile2, FileMode.Open);
-
-            // Check the file sizes. If they are not the same, the files 
-            // are not the same.
-            if (fs1.Length != fs2.Length)
+            catch (Exception ex)
             {
-                // Close the file
-                fs1.Close();
-                fs2.Close();
-
-                // Return false to indicate files are different
+                Console.WriteLine($"Error comparing files: {ex.Message}");
                 return false;
             }
-
-            // Read and compare a byte from each file until either a
-            // non-matching set of bytes is found or until the end of
-            // file1 is reached.
-            do
-            {
-                // Read one byte from each file.
-                file1byte = fs1.ReadByte();
-                file2byte = fs2.ReadByte();
-            }
-            while ((file1byte == file2byte) && (file1byte != -1));
-
-            // Close the files.
-            fs1.Close();
-            fs2.Close();
-
-            // Return the success of the comparison. "file1byte" is 
-            // equal to "file2byte" at this point only if the files are 
-            // the same.
-            return ((file1byte - file2byte) == 0);
         }
+
+        private static string NormalizeLineEndings(string text) => text.Replace("\r\n", "\n");
+
     }
 }
